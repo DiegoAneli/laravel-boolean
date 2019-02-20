811cd19d8cdf8d5bd14b7e3d541d353204ef75f6
DiegoAneli <diegoaneli@gmail.com>
2019-02-20 19:28:59 +0100

    navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <title>Laravel Boolean</title>
+  </head>
+  <body>
+
+    <header>
+      <div class="header-left">
+        <img src="https://www.boolean.careers/images/common/logo.png" alt="">
+      </div>
+      <div class="header-right">
+        <ul>
+          <li class="active"><a href="#">HOME</a></li>
+          <li><a href="#">CORSO</a></li>
+          <li><a href="#">METODO</a></li>
+          <li><a href="#">DOPO IL CORSO</a></li>
+          <li><a href="#">FAQ</a></li>
+          <div class="btn btn-primary">Iscriviti</div>
+        </ul>
+
+
+
+      </div>
+    </header>
+
+  </body>
+</html>
