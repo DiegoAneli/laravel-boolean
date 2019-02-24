f05e4f628e3a1776f6d548ee1906beb079e74d50
DiegoAneli <diegoaneli@gmail.com>
2019-02-24 21:29:06 +0100

    config array students

---
+++ config/students.php
@@ -0,0 +1,57 @@
+<?php
+
+  return [
+
+        [
+            'name' => 'Alessandro Biagini',
+            'age' => 24,
+            'azienda' => 'Softec Spa',
+            'img' => "https://www.boolean.careers/images/students/biagini.png"
+        ],
+
+        [
+            'name' => 'Samuel Poma',
+            'age' => 26,
+            'azienda' => 'Freelance Developer',
+            'img' => "https://www.boolean.careers/images/students/poma.png"
+        ],
+
+        [
+            'name' => 'Davide Lecci',
+            'age' => 28,
+            'azienda' => 'Facile.it',
+            'img' => "https://www.boolean.careers/images/students/lecci.png"
+        ],
+
+        [
+            'name' => 'Loana Masetti',
+            'age' => 35,
+            'azienda' => 'Cayenne Web Agency',
+            'img' => "https://www.boolean.careers/images/students/masetti.png"
+        ],
+
+        [
+            'name' => 'Federico Losi',
+            'age' => 30,
+            'azienda' => 'Facile.it',
+            'img' => "https://www.boolean.careers/images/students/losi.png"
+        ],
+
+        [
+            'name' => 'Ervin Bani',
+            'age' => 31,
+            'azienda' => 'Fabbrica Digitale',
+            'img' => "https://www.boolean.careers/images/students/bani.png"
+        ],
+
+        [
+            'name' => 'Alessandro Lausdei',
+            'age' => 27,
+            'azienda' => 'One Day',
+            'img' => "https://www.boolean.careers/images/students/lausdei.png"
+        ]
+
+      ];
+
+
+ ?>
